f0d915d75c659d907507b6c20e5b7eebc94b841e
veranyc <veranyc@gmail.com>
2017-08-15 17:31:20 -0400

    hmmm

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Header, Grid } from 'semantic-ui-react'
+import { Container, Header, Grid, Image } from 'semantic-ui-react'
 
 
 export default class Event extends React.Component{
@@ -9,7 +9,7 @@ export default class Event extends React.Component{
        <div>
          <Container text style={{ marginTop: '7em' }}>
            <Header as='h1'>The White Stripes</Header>
-           <Grid divided="vertically">
+           <Grid celled divided="vertically">
              <Grid.Column width={8}>
                <h4>February 2, 2005</h4>
                <h4>Venue:  Masonic Temple Theatre</h4>
@@ -30,14 +30,15 @@ export default class Event extends React.Component{
              </Grid.Column>
              <Grid.Column width={8}>
                 <Container>
-                  <h4>Notes</h4>
+                  <h2>Notes</h2>
                   <p>This was the last home show ever for our hometown band. The energy of the crowd as matched with the energy coming off the stage.
                   </p>
                 </Container>
                 <br></br>
                 <br></br>
                 <Container>
-                  <h4>Pictures</h4>
+                  <h2>Pictures</h2>
+                  <Image src='./stripesmeg.jpg' size='small' wrapped/>
                 </Container>
               </Grid.Column>
            </Grid>
